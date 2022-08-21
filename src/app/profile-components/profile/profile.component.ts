62907934ae0cf37361deccbc008d6ca8cb955ee0
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2022-08-21 21:04:29 +0200

    Forms for set profile

--- src/app/profile-components/profile/profile.component.ts
+++ src/app/profile-components/profile/profile.component.ts
@@ -5,13 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
 
-  profile: { name: string, element: string, skills: {power: number, health: number, def: number}}[] = [{
+export class ProfileComponent implements OnInit {
+  pokemonName: string = '';
+  pokemonElement: string = '';
+  power: number = 0;
+  health: number = 0;
+  def: number = 0;
+  addPokemonStatus: boolean = false;
+
+  profile: { name: string, element: string, skills: { power: number, health: number, def: number } }[] =
+    [{
     name: 'Pikachu',
     element: 'Fire',
-    skills: 
-      {
+    skills: {
         power: 280,
         health: 198,
         def: 300
@@ -22,4 +29,21 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  setProfile() {
+    setTimeout(() => {
+      this.addPokemonStatus = true;
+    }, 4000)
+  }
+
+  addPokemon() {
+    this.profile.push({
+      name: this.pokemonName,
+      element: this.pokemonElement,
+      skills: {
+        power: this.power,
+        health: this.health,
+        def: this.def,
+      }
+    });
+  }
 }
