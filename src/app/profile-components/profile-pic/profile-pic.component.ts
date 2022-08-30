886268aca9a3840b989d12c8aacf414f2ec88fed
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2022-08-31 01:58:43 +0200

    Styles element

--- src/app/profile-components/profile-pic/profile-pic.component.ts
+++ src/app/profile-components/profile-pic/profile-pic.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-pic.component.scss']
 })
 export class ProfilePicComponent implements OnInit {
-
+  img: string [] = [
+    '../../assets/img/img-pokemon-01.jpg',
+    '../../assets/img/img-pokemon-02.jpg',
+  ]
   constructor() { }
 
   ngOnInit(): void {
